215e770b439db18f01dd6657dccfb7a5fab4368e
songhwee1 <songhwee1@naver.com>
2024-03-28 16:28:58 +0900

    feat: Initial setting - Initial setting for develop IOS application.

---
+++ practice/practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by NGL Transportation on 2024/03/28.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
